0f40a4b95f8ad4347a192106d7b5ee4ae529bffc
arthurgregoryo <arthurgregorioleal@gmail.com>
2020-03-20 21:22:08 -0300

    Create install.sh for Linux Users

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd src/
+javac call/*.java
+javac decoders/*.java
+javac files/*.java
+javac identifier/*.java
+javac treatment/*.java
+javac *.java
+jar cvfm ../mips32-decode.jar META-INF/Manifest.mf call/Mips32Decode.class decoders/DecodeOpcode.class decoders/DecodeRegister.class files/ReadArchive.class files/WriteArchive.class files/Test.class identifier/RegisterIdentifier.class treatment/BinaryConversor.class treatment/InstructionTreatment.class MIPS32.class
